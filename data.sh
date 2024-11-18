476e32832b6b12a7819f3ef0ee1aa3e159f1103e
Bhanu3487 <141207558+Bhanu3487@users.noreply.github.com>
2024-11-19 01:13:08 +0530

    Added files for simulating Hurrican Matthew

---
+++ data.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd WPS/DATA/Man_Static_hres
+
+# albedo_modis
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/albedo_modis.tar.bz2
+# greenfrac_fpar_modis
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/greenfrac_fpar_modis.tar.bz2
+# lai_modis_10m
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/lai_modis_10m.tar.bz2
+# lai_modis_30s
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/lai_modis_30s.tar.bz2
+# maxsnowalb_modis
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/maxsnowalb_modis.tar.bz2
+# modis_landuse_20class_30s_with_lakes
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/modis_landuse_20class_30s_with_lakes.tar.bz2
+# orogwd_2deg
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/orogwd_2deg.tar.bz2
+# orogwd_1deg
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/orogwd_1deg.tar.bz2
+# orogwd_30m
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/orogwd_30m.tar.bz2
+# orogwd_20m
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/orogwd_20m.tar.bz2
+# orogwd_10m
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/orogwd_10m.tar.bz2
+# soiltemp_1deg
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltemp_1deg.tar.bz2
+# soiltype_bot_30s
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_bot_30s.tar.bz2
+# soiltype_top_30s
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_top_30s.tar.bz2
+# topo_gmted2010_30s
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/topo_gmted2010_30s.tar.bz2
+# varsso
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/varsso.tar.bz2
+# varsso_10m
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/varsso_10m.tar.bz2
+# varsso_5m
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/varsso_5m.tar.bz2
+#varsso_2m
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/varsso_2m.tar.bz2

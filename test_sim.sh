7561f0d7da52ca98c33a60a80f28a94b73b13673
Bhanu3487 <141207558+Bhanu3487@users.noreply.github.com>
2024-12-06 11:51:33 +0530

    Create test_sim.sh

---
+++ test_sim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define file paths directly
+file1="/home/bhanu.repalle/Output_files/wrfout_d01_2016-10-06_00:00:00" #expected
+file2="/wrf/WRF/test/em_real/wrfout_d01_2016-10-06_00:00:00" #simularion output
+
+# Use ncdump to compare the two NetCDF files
+ncdump -c "$file1" > file1.txt
+ncdump -c "$file2" > file2.txt
+
+# Compare the two output files
+if diff -q file1.txt file2.txt > /dev/null; then
+  echo "The NetCDF files are identical."
+else
+  echo "The NetCDF files are different."
+fi
+
+# Clean up temporary files
+rm file1.txt file2.txt

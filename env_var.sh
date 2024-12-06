78c7503215cd5b6e448a3c398845e937cb21e710
Bhanu3487 <141207558+Bhanu3487@users.noreply.github.com>
2024-12-06 12:11:06 +0530

    Update env_var.sh

--- env_var.sh
+++ env_var.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Add the library paths to .bashrc
-echo 'export LD_LIBRARY_PATH=/wrf/Build_WRF/LIBRARIES/grib2/lib:/wrf/Build_WRF/LIBRARIES/grib2/bin:/wrf/Build_WRF/LIBRARIES/netcdf/lib:/wrf/Build_WRF/LIBRARIES/mpich/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-echo 'export C_INCLUDE_PATH=/wrf/Build_WRF/LIBRARIES/grib2/include:/wrf/Build_WRF/LIBRARIES/netcdf/include:/wrf/Build_WRF/LIBRARIES/mpich/include:$C_INCLUDE_PATH' >> ~/.bashrc
-echo 'export PATH=/wrf/Build_WRF/LIBRARIES/grib2/bin:/wrf/Build_WRF/LIBRARIES/mpich/bin:$PATH' >> ~/.bashrc
+export LD_LIBRARY_PATH=/wrf/Build_WRF/LIBRARIES/grib2/lib:/wrf/Build_WRF/LIBRARIES/grib2/bin:/wrf/Build_WRF/LIBRARIES/netcdf/lib:/wrf/Build_WRF/LIBRARIES/mpich/lib:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=/wrf/Build_WRF/LIBRARIES/grib2/include:/wrf/Build_WRF/LIBRARIES/netcdf/include:/wrf/Build_WRF/LIBRARIES/mpich/include:$C_INCLUDE_PATH
+export PATH=/wrf/Build_WRF/LIBRARIES/grib2/bin:/wrf/Build_WRF/LIBRARIES/mpich/bin:/wrf/Build_WRF/LIBRARIES/netcdf/bin:$PATH
 
 # Apply the changes
 source ~/.bashrc
